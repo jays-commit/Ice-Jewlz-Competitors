a49811f0619d18e7c96106b9cb6655d3c42a3595
Jesse O <97202317+jays-commit@users.noreply.github.com>
2022-02-08 04:22:17 +0000

    automation basics

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
 import time
 
 PATH = '/Users/jesseocean/Documents/Python Lessons/Chromedriver/chromedriver'
+chrono = "https://www.chrono24.co.uk/"
+a_j = "https://www.instagram.com/a_jewellers/?hl=en"
+ice = 'https://bryanni.com/'
 
 s = Service(PATH)
 browser = webdriver.Chrome(service=s)
-url = 'https://www.google.com'
-browser.get(url)
-time.sleep(5)
+browser.get(ice)
+
+print()
+print(browser.title)
+
+# search = browser.find_elements_by_class_name("prestige--v4")
+# search.send_keys("rolex")
+# search.send_keys(Keys.RETURN)
+
+
+time.sleep(3)
 browser.quit()
